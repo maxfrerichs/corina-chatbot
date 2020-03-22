50b45caa8a9fc0758a8e1134f9565cab94666f68
Xaver Müller <you@example.com>
2020-03-22 03:10:01 +0100

    fixed authorization

--- requestscript.php
+++ requestscript.php
@@ -2,7 +2,6 @@
 /*     error_reporting(E_ALL);
     ini_set("display_errors", 1); */
 
-    $api_key = 'd-Pd1HsNUFruREq-PhyDgNeYlERExUbR69KukVkMz1lZ';
     $skill_id = 'f9a32bd3-0cce-441c-af2a-94bdddd1f469';
     $baseUrl = "https://api.eu-de.assistant.watson.cloud.ibm.com/instances/80b729fa-6067-4885-9f02-ad6fecba0e1a/v2/assistants/$api_key";
     $sessionId = $_POST['session_id'] != null ? $POST['session_id'] : null;
@@ -31,6 +30,7 @@
     }
     
     function makeRequest($config) {
+        $api_key = 'd-Pd1HsNUFruREq-PhyDgNeYlERExUbR69KukVkMz1lZ';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_USERPWD, "apikey:" . $api_key);
