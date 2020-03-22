206039e1b358470531c1f6d0971385aca5a4cf3b
Xaver Müller <you@example.com>
2020-03-22 03:39:08 +0100

    fixed empty url on session request

--- requestscript.php
+++ requestscript.php
@@ -2,8 +2,8 @@
 /*     error_reporting(E_ALL);
     ini_set("display_errors", 1); */
 
-    $skill_id = 'f9a32bd3-0cce-441c-af2a-94bdddd1f469';
-    $baseUrl = "https://api.eu-de.assistant.watson.cloud.ibm.com/instances/80b729fa-6067-4885-9f02-ad6fecba0e1a/v2/assistants/$api_key";
+    $assistant_id = 'f9a32bd3-0cce-441c-af2a-94bdddd1f469';
+    $baseUrl = "https://api.eu-de.assistant.watson.cloud.ibm.com/instances/80b729fa-6067-4885-9f02-ad6fecba0e1a/v2/assistants/$assistant_id";
     $sessionId = $_POST['session_id'] != null ? $POST['session_id'] : null;
     $requestUrl = $baseUrl."/sessions/$sessionId/message?version=2020-02-05";
     $sessionUrl = $baseUrl."/sessions?version=2020-02-05";
@@ -15,17 +15,15 @@
             CURLOPT_POST => true,
             CURLOPT_POSTFIELDS => $_POST
         );
-        /* print_r($config, true); */
         echo makeRequest($config);
     } else {
         $params = $_POST;
-        $params['session_id'] = getsessionId();
+        $params['session_id'] = getsessionId()['session_id'];
         $config = array(
             CURLOPT_URL => $requestUrl,
             CURLOPT_POST => true,
             CURLOPT_POSTFIELDS => $params
         );
-        /* print_r($config, true); */
         echo makeRequest($config);
     }
     
@@ -42,6 +40,9 @@
 
 
     function getsessionId() {
+        $assistant_id = 'f9a32bd3-0cce-441c-af2a-94bdddd1f469';
+        $baseUrl = "https://api.eu-de.assistant.watson.cloud.ibm.com/instances/80b729fa-6067-4885-9f02-ad6fecba0e1a/v2/assistants/$assistant_id";
+        $sessionUrl = $baseUrl."/sessions?version=2020-02-05";
         $config = array(
             CURLOPT_URL => $sessionUrl,
             CURLOPT_POST => true
